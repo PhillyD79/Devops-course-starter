c00ef1c7775c7fa197d2c09fb5505fcdab940538
PhillyD79 Durkan <phillip.durkan@asda.co.uk>
2024-02-27 14:30:24 +0000

    Show homepage with items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,6 @@
-from flask import Flask
-
+from flask import Flask, render_template
 from todo_app.flask_config import Config
+from todo_app.data.session_items import get_items
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -8,4 +8,5 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    items = get_items()
+    return render_template("index.html", template_items = items)
