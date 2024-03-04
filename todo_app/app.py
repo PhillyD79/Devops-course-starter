6a2a52078cd7206e2a6f168c3f530b24f78ebfab
PhillyD79 Durkan <phillip.durkan@asda.co.uk>
2024-03-04 14:11:48 +0000

    Clean up

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request , redirect
 from todo_app.flask_config import Config
-from todo_app.data.session_items import get_items
+from todo_app.data.session_items import get_items, add_item
 
 app = Flask(__name__)
 app.config.from_object(Config())
