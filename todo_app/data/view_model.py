0e547fe27cb00c9e9a4c0e9216ae93a2a3007f7b
PhillyD79 Durkan <phillip.durkan@asda.co.uk>
2024-09-18 14:44:58 +0000

    Add todo_items and doing_items

--- todo_app/data/view_model.py
+++ todo_app/data/view_model.py
@@ -8,12 +8,24 @@ class ViewModel:
 
     @property
     def todo_items(self):
-        return []
+        output = []
+
+        for item in self._items:
+            if item.status == "To Do"
+                output.append(item)
 
     @property
     def doing_items(self):
-        return []
+        output = []
+
+        for item in self._items:
+            if item.status == "Doing"
+                output.append(item)
 
     @property
-    def done_items(self):
-        return []
+    def done_items(self): -> list[Item]:
+        output = []
+
+        for item in self._items:
+            if item.status == "Done"
+                output.append(item)
