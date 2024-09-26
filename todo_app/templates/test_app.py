59540007c780f714eb0251d376feed8b5325fa57
PhillyD79 Durkan <phillip.durkan@asda.co.uk>
2024-09-26 11:11:29 +0000

    fix indentation

--- todo_app/templates/test_app.py
+++ todo_app/templates/test_app.py
@@ -4,16 +4,16 @@ from todo_app import app
 
 @pytest.fixture
 def client():
-    # Use our test integration config instead of the 'real' version
-    file_path = find_dotenv('.env.test')
-    load_dotenv(file_path, override=True)
+        # Use our test integration config instead of the 'real' version
+        file_path = find_dotenv('.env.test')
+        load_dotenv(file_path, override=True)
 
-    # Create the new app.
-    test_app = app.create_app()
+        # Create the new app.
+        test_app = app.create_app()
 
-    # Use the app to create a test_client that can be used in our tests.
-    with test_app.test_client() as client:
-        yield client
+        # Use the app to create a test_client that can be used in our tests.
+        with test_app.test_client() as client:
+            yield client
 
 def test_index_page(client):
-    response = client.get('/')
+        response = client.get('/')
